7c554f3bd459f1f532b9a95bd63394dcd2fa7745
AFAlina190 <afalina190@gmail.com>
2024-04-16 12:12:38 +0200

    12

--- frontendMain/homework/hw_11/practice_hw_11_script.js
+++ frontendMain/homework/hw_11/practice_hw_11_script.js
@@ -1,39 +1,46 @@
-const btn = document.getElementById("btn1");
-console.log(btn);
+// const btn = document.getElementById("btn1");
+// console.log(btn);
 
-// =================================================
-const heading1 = document.createElement("h1");
-console.log(heading1);
+// // =================================================
+// const heading1 = document.createElement("h1");
+// console.log(heading1);
 
-// =================================================
-// создадим заголовок
-const heading = document.createElement("h2");
+// // =================================================
+// // создадим заголовок
+// const heading = document.createElement("h2");
 
-// создадим тексовый узел
- heading_text = document.createTextNode("ПРОДОЛЖИМ ИЗУЧАТЬ JS");
+// // создадим тексовый узел
+//  heading_text = document.createTextNode("ПРОДОЛЖИМ ИЗУЧАТЬ JS");
 
-// прикрепим к заголовку текст
-heading.appendChild(heading_text);
+// // прикрепим к заголовку текст
+// heading.appendChild(heading_text);
 
-// рикрепим готовы заголовок к body документа
-document.body.appendChild(heading);
+// // рикрепим готовы заголовок к body документа
+// document.body.appendChild(heading);
 
-// ==================================================
+// // ==================================================
 
-let div2 = document.createElement('h2');
-h2.innerText = 'WE ARE DOING GOOD';
+// let div2 = document.createElement('h2');
+// h2.innerText = 'WE ARE DOING GOOD';
 
-// =================================================
+// // =================================================
 
-const banana = document.getElementById('banana');
-banana.style.color = "orange";
-// =================================================
+// const banana = document.getElementById('banana');
+// banana.style.color = "orange";
+// // =================================================
 
-let el = document.createElement("div3");
-div.innerText =  "YOU ARE THE BEST";
-el.textContent = "Just text";
+// let el = document.createElement("div3");
+// div.innerText =  "YOU ARE THE BEST";
+// el.textContent = "Just text";
 
 
+const list = document.querySelector(".list");
+
+const newTechnologies = ["React", "TyprScript","Node.js"];
+const markup = newTechnologies.map((technology)=>`<li class="list-item new">${technology}</li>`).join("");
+
+list.insertAdjacentHTML("beforeend", markup);
+list.insertAdjacentHTML("beforebegin", "<h2>Popular technologies</h2>");
 
 
 
