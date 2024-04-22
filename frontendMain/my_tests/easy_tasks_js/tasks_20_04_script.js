c4e8ca29155c162f731cf5aafbc5541ef4b9e40b
AFAlina190 <afalina190@gmail.com>
2024-04-22 21:56:39 +0200

    lesson 17

---
+++ frontendMain/my_tests/easy_tasks_js/tasks_20_04_script.js
@@ -0,0 +1,56 @@
+// вывести числа от 0 до 10 с помощью цыклов
+for (let i = 0; i <= 10; i++) {
+    //console.log(i);     
+}
+
+let num = 1;
+
+while (num <= 10) {
+    //console.log(num);
+    num++
+}
+
+// Задача: Вывести четные числа от 1 до 20.
+
+for (let i = 2; i <= 100; i += 2) {
+    // console.log(i);
+}
+
+let num2 = 2;
+while (num2 <= 20) {
+    // console.log(num2);
+    num2 += 2;
+    
+}
+
+// Вычислить сумму чисел от 1 до 100.
+let sum = 0;
+
+for (let i = 1; i <= 100; i++) {
+    sum+=i;
+
+}
+console.log(sum);
+
+// ===========================
+
+let sum1= 0;
+let num1= 1;
+
+while (num1<=100) {
+    sum1+=num1;
+    num1++;
+}
+// console.log(sum1);
+
+// ===========================================================
+document.getElementById("myButton").addEventListener("click", function () {
+    document.getElementById("demo").innerHTML = "HELLO JS!!";
+}) 
+
+
+
+
+
+
+
